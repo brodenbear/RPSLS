f344c26afad6c9056c302c68942556699dd8120a
Broden Berrett <brodenb@gmail.com>
2023-05-08 10:56:20 -0600

    players can now choose a gesture and gestures compare properly

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -45,12 +46,48 @@ namespace RPSLS
         }
         public void CreatePlayerObjects(int numberOfHumanPlayers)
         {
+            if (numberOfHumanPlayers == 1)
+            {
+                Console.WriteLine("Please enter your name:");
+                string playerName = Console.ReadLine();
+                playerOne = new HumanPlayer(playerName);
+                playerTwo = new ComputerPlayer();
+            }
+            else
+            {
+                Console.WriteLine("Player 1, please enter your name:");
+                string playerOneName = Console.ReadLine();
+                playerOne = new HumanPlayer(playerOneName);
 
+                Console.WriteLine("Player 2, please enter your name:");
+                string playerTwoName = Console.ReadLine();
+                playerTwo = new HumanPlayer(playerTwoName);
+            }
         }
 
         public void CompareGestures()
         {
+            Console.WriteLine($"Player 1 chose {playerOne.chosenGesture}.");
+            Console.WriteLine($"Player 2 chose {playerTwo.chosenGesture}.");
 
+            if (playerOne.chosenGesture == playerTwo.chosenGesture)
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            else if ((playerOne.chosenGesture == "rock" && playerTwo.chosenGesture == "scissors" || "lizard") ||
+                (playerOne.chosenGesture == "paper" && playerTwo.chosenGesture == "rock" || "spock") ||
+                (playerOne.chosenGesture == "scissors" && playerTwo.chosenGesture == "paper" || "lizard")||
+                (playerOne.chosenGesture == "lizard" && playerTwo.chosenGesture == "paper" || "spock"
+                ) || (playerOne.chosenGesture =="spock" &&  playerTwo.chosenGesture == "scissors" || "rock")
+            {
+                Console.WriteLine("Player 1 wins the round!");
+                playerOne.score++;
+            }
+            else
+            {
+                Console.WriteLine("Player 2 wins the round!");
+                playerTwo.score++;
+            }
         }
 
         public void DisplayGameWinner()
--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -18,13 +18,50 @@ namespace RPSLS
         public Player(string name)
         {
             this.name = name;
-            gestures = new List<string> {"rock", "paper", "scissors", "lizard", "Spock" };
+             gestures = new List<string> { "rock", "paper", "scissors", "lizard", "Spock" };
             chosenGesture = "";
             score = 0;
         }
-
         //Member Methods (CAN DO)
         //This abstract method must be overridden by the child Player classes
         public abstract void ChooseGesture();
+    }
+
+    internal class HumanPlayer : Player
+    {
+        //Constructor
+        public HumanPlayer(string name) : base(name)
+        {
+        }
+
+        public override void ChooseGesture()
+        {
+            Console.WriteLine($"{name}, please choose a gesture:");
+            foreach (var gesture in gestures)
+            {
+                Console.WriteLine($"- {gesture}");
+            }
+            chosenGesture = Console.ReadLine().ToLower();
+            while (!gestures.Contains(chosenGesture))
+            {
+                Console.WriteLine("Invalid gesture, please try again.");
+                chosenGesture = Console.ReadLine().ToLower();
+            }
+        }
+    }
+
+    internal class ComputerPlayer : Player
+    {
+        //Constructor
+        public ComputerPlayer() : base("Computer")
+        {
+        }
+
+        public override void ChooseGesture()
+        {
+            Random random = new Random();
+            int index = random.Next(gestures.Count);
+            chosenGesture = gestures[index];
+        }
     }
 }
