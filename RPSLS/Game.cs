8f82b2f791e1b8af868aa9f510720803bbd0a156
Broden Berrett <brodenb@gmail.com>
2023-05-07 18:11:53 -0600

    Show Welcome Message and prompt for number of players

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -21,12 +21,22 @@ namespace RPSLS
         //Member Methods (CAN DO)
         public void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
+            Console.WriteLine("Welcome to RPSLS! Here are the rules:\nFirst player to 3 points, wins!\nRock crushes Scissors\nScissors cuts Paper \nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n ");
         }
 
         public int ChooseNumberOfHumanPlayers()
         {
-            return 0;
+            Console.WriteLine("Will you be playing with 1 or 2 players?");
+            string numberOfPlayers = Console.ReadLine();
+            if (int.TryParse(numberOfPlayers, out int playerNumber))
+            {
+                Console.WriteLine($"You have chosen {playerNumber} players");
+            }
+            else 
+            { 
+                Console.WriteLine($"You have picked an incorrect number of players, {playerNumber} is not between 1 and 2"); 
+            }
+            return playerNumber;
         }
 
         public void CreatePlayerObjects(int numberOfHumanPlayers)
@@ -47,6 +57,7 @@ namespace RPSLS
         public void RunGame()
         {
             WelcomeMessage();
+
         }
     }
 }
